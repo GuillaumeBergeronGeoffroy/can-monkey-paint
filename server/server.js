31a6c189eebe28e11b3b7d3ac9df435156bc37d0
GuillaumeBergeronGeoffroy <geoffroy_5@hotmail.com>
2022-01-10 20:14:01 -0500

    Update structure

--- server/server.js
+++ server/server.js
@@ -7,19 +7,6 @@ const Server = {
     routes: {
         ConnManager: ConnManager
     },
-    run: () => {
-        Server.server = new WebSocketServer({
-            port: 8080,
-        });
-        Server.server.on('connection', (ws) => {
-            ws.on('message',  async (data) => {
-                data = JSON.parse(data);
-                data.ws = ws;
-                let result = data.route ? await Server.routes[data.route].handleMessage(data) : null
-                result && result.resolve && Server.resolver[result.resolve] ? Server.resolver[result.resolve](JSON.stringify(result.data), result.ws) : null
-            });
-        });
-    },
     resolver: {
         broadcast: (data, ws = null) => {
             wss.clients.forEach(client => {
@@ -32,6 +19,17 @@ const Server = {
             ws.send(data);
         }
     },
-    
+    run: () => {
+        Server.server = new WebSocketServer({
+            port: 8080,
+        });
+        Server.server.on('connection', (ws) => {
+            ws.on('message',  async (data) => {
+                data = {...JSON.parse(data), ws:ws };
+                let result = data.route ? await Server.routes[data.route].handleMessage(data) : null
+                result && result.resolve && Server.resolver[result.resolve] ? Server.resolver[result.resolve](JSON.stringify(result.data), result.ws) : null
+            });
+        });
+    },
 }
 Server.run();
