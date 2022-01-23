811d01cc88b736ff0de733452eabfc2451a5b414
GuillaumeBergeronGeoffroy <ohbergui@gmail.com>
2022-01-23 17:15:55 -0500

    Fix iceConfig syntax

--- app/js/client.js
+++ app/js/client.js
@@ -91,42 +91,7 @@ const Client = {
     WebRTC: {
         // STUN server config for Internet Connectivity Establishment without TURN for now
         iceConfig: {
-            iceServers: [
-                { url: 'stun:stun01.sipphone.com' },
-                { url: 'stun:stun.ekiga.net' },
-                { url: 'stun:stun.fwdnet.net' },
-                { url: 'stun:stun.ideasip.com' },
-                { url: 'stun:stun.iptel.org' },
-                { url: 'stun:stun.rixtelecom.se' },
-                { url: 'stun:stun.schlund.de' },
-                { url: 'stun:stun.l.google.com:19302' },
-                { url: 'stun:stun1.l.google.com:19302' },
-                { url: 'stun:stun2.l.google.com:19302' },
-                { url: 'stun:stun3.l.google.com:19302' },
-                { url: 'stun:stun4.l.google.com:19302' },
-                { url: 'stun:stunserver.org' },
-                { url: 'stun:stun.softjoys.com' },
-                { url: 'stun:stun.voiparound.com' },
-                { url: 'stun:stun.voipbuster.com' },
-                { url: 'stun:stun.voipstunt.com' },
-                { url: 'stun:stun.voxgratia.org' },
-                { url: 'stun:stun.xten.com' },
-                {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=tcp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                }
-            ]
+            'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }]
         },
         createConnection: () => {
             let connection = {
@@ -175,16 +140,11 @@ const Client = {
             });
         },
         registerConnectionEvents: (peerConnection) => {
-            peerConnection.addEventListener('icecandidate', event => {
-                if (event.candidate) {
-                    console.log('candidate')
-                }
-            });
             peerConnection.onconnectionstatechange = (e) => console.log(e)
             peerConnection.onsignalingstatechange = (e) => console.log(e)
             peerConnection.onicegatheringstatechange = (e) => console.log(e)
             peerConnection.onicecandidateerror = (e) => console.log(e)
-            peerConnection.onaddstream = (e) => console.log(e)
+            peerConnection.ontrack = (e) => console.log(e)
             peerConnection.onnegotiationneeded = (e) => console.log(e)
         },
         registerChannelEvent: (sendChannel) => {
